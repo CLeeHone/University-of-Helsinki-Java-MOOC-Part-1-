9876ba8f6e020747766610aaea9edf84d5222a57
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-03 14:44:40 -0400

    Add CheckingTheAge class

---
+++ Part01_35_CheckingTheAge/src/CheckingTheAge.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class CheckingTheAge {
+ 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("How old are you?");
+        double givenAge = Double.valueOf(scan.nextLine());
+        
+        if (givenAge >= 0 && givenAge <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+    }
+}
