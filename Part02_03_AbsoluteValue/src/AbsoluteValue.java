873665966e7e14665357edfe536bc9f2cf6166e6
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-03 15:24:24 -0400

    Add AbsoluteValue class

---
+++ Part02_03_AbsoluteValue/src/AbsoluteValue.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class AbsoluteValue {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int givenNumber = Integer.valueOf(scanner.nextLine());
+        
+        if (givenNumber < 0) {
+            System.out.println(givenNumber * -1);
+        } else {
+            System.out.println(givenNumber);
+        }
+    }
+}
