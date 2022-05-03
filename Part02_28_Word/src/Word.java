6e6f1ca96f5d107309651d004c041357bc1977c2
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-03 18:43:35 -0400

    Add Word class

---
+++ Part02_28_Word/src/Word.java
@@ -0,0 +1,11 @@
+public class Word {
+ 
+    public static void main(String[] args) {
+        String word = word();
+        System.out.println(word);
+    }
+ 
+    public static String word() {
+        return "Hello hello!";
+    }
+}
