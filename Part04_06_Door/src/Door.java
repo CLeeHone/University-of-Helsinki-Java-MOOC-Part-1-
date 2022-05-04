baef5ccd9f4c399cfef3e1c6382b1219ae4b90c2
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-03 21:18:36 -0400

    Add Door and Main classes

---
+++ Part04_06_Door/src/Door.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+public class Door {
+    //no instance variables
+    //Create for it a constructor with no parameters (or use the default constructor)
+    public Door () {    
+    }
+    
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
+ 
