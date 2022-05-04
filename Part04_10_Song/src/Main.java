2bee2e285e200ec82a571b16dbd5bb6cc3117be5
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-03 21:34:24 -0400

    Add Song and Main classes

---
+++ Part04_10_Song/src/Main.java
@@ -0,0 +1,12 @@
+ 
+public class Main {
+ 
+  public static void main(String[] args) {
+    // This is just an empty main method, where you can test your
+    // Song class. You can for instance try this:
+ 
+    Song nocturne = new Song("Nocturne No.2", 273);
+    System.out.println("The song " + nocturne.name() + " has a length of " + nocturne.length() + " seconds.");
+  }
+  
+}
