96e48d57e32f6e289e601ec5b80d8e650f063d29
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-04 12:51:58 -0400

    Add PaymentCard, PaymentTerminal, and Main classes

---
+++ Part05_10_CardPayments/src/PaymentCard.java
@@ -0,0 +1,28 @@
+ public class PaymentCard {
+ 
+    private double balance;
+ 
+    public PaymentCard(double balance) {
+        this.balance = balance;
+    }
+ 
+    public double balance() {
+        return balance;
+    }
+ 
+    public void addMoney(double increase) {
+        balance += increase;
+    }
+ 
+    public boolean takeMoney(double amount) {
+        // implement the method so that it only takes money from the card if
+        // the balance is at least the amount parameter.
+        // returns true if successful and false otherwise
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
+ 
